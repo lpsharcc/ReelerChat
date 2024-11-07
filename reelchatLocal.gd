74c3069705529bedd9c43868734c1146e3906d0c
Blueberry <blue@blueberry.coffee>
2024-11-07 11:50:22 -0700

    remove unnecessary logic to send local steam id

--- reelchatLocal.gd
+++ reelchatLocal.gd
@@ -74,7 +74,7 @@ func _process(_delta: float)->void :
 	check_for_voice()
 
 func _send_net(type: String, data: Dictionary = {}, target: String = "all"):
-	var complete_data: Dictionary = { "steamid": Network.STEAM_ID, "type": ("reelchat_%s" % type), "voice_data": data}
+	var complete_data: Dictionary = { "type": ("reelchat_%s" % type), "voice_data": data}
 	
 	Network._send_P2P_Packet(complete_data, target, 2, 5)
 
